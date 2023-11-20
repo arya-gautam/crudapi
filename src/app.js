93f5a18a06ad15384ae3e868b8db0fe840c815f4
Gautamarya <er.gautamarya@gmail.com>
2023-11-20 16:43:55 +0530

    update

--- src/app.js
+++ src/app.js
@@ -122,11 +122,11 @@ app.get("/",cors(corsOptions), async (req,res)=>{
             });
 
             var config = {
-                key: process.env.EASEBUZZ_KEY,
-                salt: process.env.EASEBUZZ_SALT,
-                env: process.env.EASEBUZZ_ENV,
-                enable_iframe: process.env.EASEBUZZ_IFRAME,
-              };
+              key: process.env.EASEBUZZ_KEY || '2PBP7IABZ2',
+              salt: process.env.EASEBUZZ_SALT || 'DAH88E3UWQ',
+              env: process.env.EASEBUZZ_ENV || 'test',
+              enable_iframe: process.env.EASEBUZZ_IFRAME || 0,
+            };
               
               
               
